82d37e5c73d93625f31ddc84674fbfdffbcde159
daniel chceca <dcheca@umariana.edu.co>
2021-10-20 19:03:42 -0500

    personaje 3D
    personaje con animaciones.

---
+++ personajeChek/PersonajeTheboss/Assets/controlPersonaje.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlPersonaje : MonoBehaviour
+{
+    public float velocidadPersonaje = 5.0f;
+    public float rotarPersonaje = 300.0f;
+    private Animator anim;
+
+    public float movimientox, movimientoy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movimientox = Input.GetAxis("Horizontal");
+        movimientoy = Input.GetAxis("Vertical");
+
+        transform.Translate(0, 0, movimientoy * Time.deltaTime * velocidadPersonaje);
+
+        transform.Rotate(0, movimientox * Time.deltaTime * rotarPersonaje, 0);
+
+        anim.SetFloat("posX", movimientox);
+        anim.SetFloat("posY", movimientoy);
+
+
+    }
+}
